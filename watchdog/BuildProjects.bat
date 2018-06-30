33c8a1c93b9e79d3218f37ae6a8da3ba5dc3dff3
Techmo <bradlytillmann@devmentality.com>
2018-06-30 14:07:18 -0600

    Configured files and names

---
+++ watchdog/BuildProjects.bat
@@ -0,0 +1,4 @@
+D:\Downloads\premake-4.4-beta5-windows\premake4 --os=windows --platform=x32 --file=BuildProjects.lua vs2010
+D:\Downloads\premake-4.4-beta5-windows\premake4 --os=macosx --platform=universal32 --file=BuildProjects.lua gmake
+D:\Downloads\premake-4.4-beta5-windows\premake4 --os=linux --platform=x32 --file=BuildProjects.lua gmake
+pause
